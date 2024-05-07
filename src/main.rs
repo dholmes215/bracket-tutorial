c72b44d3c6791dbb4fd03379e7aa9b7c434f7aff
David Holmes <dholmes@dholmes.us>
2024-05-07 15:06:44 -0500

    Don't call builder.with_automatic_console_resize() on wasm32

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,16 @@ embedded_resource!(FONT, "../resources/terminal_10x16.png");
 
 fn main() -> BError {
     link_resource!(FONT, "resources/terminal_10x16.png");
-    let context = BTermBuilder::new()
+    let builder = BTermBuilder::new()
         .with_title("Roguelike Tutorial")
-        .with_automatic_console_resize(true)
         .with_font("terminal_10x16.png", 10, 16)
         .with_tile_dimensions(10, 16)
         .with_simple_console(80, 25, "terminal_10x16.png")
-        .with_dimensions(80, 25)
-        .build()?;
+        .with_dimensions(80, 25);
+    #[cfg(not(target_arch = "wasm32"))]
+    let builder = builder.with_automatic_console_resize(true);
+    let context = builder.build()?;
+
     let gs = State{ };
     main_loop(context, gs)
 }
