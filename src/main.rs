99e0ebb9bd0b2702d5e50c4fd656dde1732e1975
David Holmes <dholmes@dholmes.us>
2024-05-07 21:15:16 -0500

    2.1: Moving the player

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use bracket_lib::prelude::*;
 use specs::prelude::*;
 use std::cmp::{max, min};
-use specs::{AccessorCow, RunningTime};
 use specs_derive::Component;
+
+const TERM_WIDTH: i32 = 80;
+const TERM_HEIGHT: i32 = 40;
 
 #[derive(Component)]
 struct Position {
@@ -18,20 +20,23 @@ struct Renderable {
 }
 
 #[derive(Component)]
-struct LeftMover{}
+struct LeftMover {}
 
 struct LeftWalker {}
 
 impl<'a> System<'a> for LeftWalker {
     type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
 
-    fn run(&mut self, (lefty, mut pos) : Self::SystemData) {
-        for (_lefty,pos) in (&lefty, &mut pos).join() {
+    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
+        for (_lefty, pos) in (&lefty, &mut pos).join() {
             pos.x -= 1;
-            if pos.x < 0 { pos.x = 79 }
+            if pos.x < 0 { pos.x = TERM_WIDTH - 1 }
         }
     }
 }
+
+#[derive(Component, Debug)]
+struct Player {}
 
 struct State {
     ecs: World,
@@ -39,7 +44,7 @@ struct State {
 
 impl State {
     fn run_systems(&mut self) {
-        let mut lw = LeftWalker{};
+        let mut lw = LeftWalker {};
         lw.run_now(&self.ecs);
         self.ecs.maintain();
     }
@@ -49,6 +54,7 @@ impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
 
+        player_input(self, ctx);
         self.run_systems();
 
         let positions = self.ecs.read_storage::<Position>();
@@ -56,6 +62,30 @@ impl GameState for State {
 
         for (pos, render) in (&positions, &renderables).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
+}
+
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(TERM_WIDTH - 1, max(0, pos.x + delta_x));
+        pos.y = min(TERM_HEIGHT - 1, max(0, pos.y + delta_y));
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut BTerm) {
+    // Player movement
+    match ctx.key {
+        None => {} // Nothing happened
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
         }
     }
 }
@@ -68,8 +98,8 @@ fn main() -> BError {
         .with_title("Roguelike Tutorial")
         .with_font("terminal_10x16.png", 10, 16)
         .with_tile_dimensions(10, 16)
-        .with_simple_console(80, 40, "terminal_10x16.png")
-        .with_dimensions(80, 40);
+        .with_simple_console(TERM_WIDTH, TERM_HEIGHT, "terminal_10x16.png")
+        .with_dimensions(TERM_WIDTH, TERM_HEIGHT);
     #[cfg(not(target_arch = "wasm32"))]
         let builder = builder.with_automatic_console_resize(true);
     let context = builder.build()?;
@@ -80,6 +110,7 @@ fn main() -> BError {
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<LeftMover>();
+    gs.ecs.register::<Player>();
 
     gs.ecs.create_entity()
         .with(Position { x: 40, y: 25 })
@@ -88,6 +119,7 @@ fn main() -> BError {
             fg: RGB::named(YELLOW),
             bg: RGB::named(BLACK),
         })
+        .with(Player {})
         .build();
 
     for i in 0..10 {
@@ -98,7 +130,7 @@ fn main() -> BError {
                 fg: RGB::named(RED),
                 bg: RGB::named(BLACK),
             })
-            .with(LeftMover{})
+            .with(LeftMover {})
             .build();
     }
 
