42cf17575e97bab94cb2cdfa27215855b831a086
David Holmes <dholmes@dholmes.us>
2024-05-07 22:42:45 -0500

    2.2: Building a simple map

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ use specs_derive::Component;
 const TERM_WIDTH: i32 = 80;
 const TERM_HEIGHT: i32 = 40;
 
+const MAP_WIDTH: i32 = TERM_WIDTH;
+const MAP_HEIGHT: i32 = TERM_HEIGHT;
+
 #[derive(Component)]
 struct Position {
     x: i32,
@@ -37,6 +40,46 @@ impl<'a> System<'a> for LeftWalker {
 
 #[derive(Component, Debug)]
 struct Player {}
+
+#[derive(PartialEq, Copy, Clone)]
+enum TileType {
+    Wall,
+    Floor,
+}
+
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * 80) + x as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; 80 * 50];
+
+    // Make the boundaries walls
+    for x in 0..MAP_WIDTH {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, MAP_HEIGHT - 1)] = TileType::Wall;
+    }
+
+    for y in 0..MAP_HEIGHT {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(MAP_HEIGHT, y)] = TileType::Wall;
+    }
+
+    // Now we'll randomly splat a bunch of walls. It won't be pretty, but it's a decent illustration.
+    // First obtain the thread-local RNG:
+    let mut rng = RandomNumberGenerator::new();
+
+    for _i in 0..400 {
+        let x = rng.roll_dice(1, MAP_WIDTH - 1);
+        let y = rng.roll_dice(1, MAP_HEIGHT - 1);
+        let idx = xy_idx(x, y);
+        if idx != xy_idx(40, 25) {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    map
+}
 
 struct State {
     ecs: World,
