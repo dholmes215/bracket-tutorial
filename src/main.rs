b7ac4a2b21cb2b65880c0106aad88ba7da7d9488
David Holmes <dholmes@dholmes.us>
2024-05-08 20:53:36 -0500

    Do some refactoring I missed in previous section

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,14 @@
 mod map;
+mod player;
+mod components;
 
 use bracket_lib::prelude::*;
 use specs::prelude::*;
-use std::cmp::{max, min};
-use specs_derive::Component;
+use components::{Player, Position, Renderable};
 use crate::map::*;
 
 const TERM_WIDTH: i32 = 80;
 const TERM_HEIGHT: i32 = 40;
-
-#[derive(Component)]
-struct Position {
-    x: i32,
-    y: i32,
-}
-
-#[derive(Component)]
-struct Renderable {
-    glyph: FontCharType,
-    fg: RGB,
-    bg: RGB,
-}
-
-#[derive(Component, Debug)]
-struct Player {}
-
-fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
-    let mut positions = ecs.write_storage::<Position>();
-    let mut players = ecs.write_storage::<Player>();
-    let map = ecs.fetch::<Map>();
-
-    for (_player, pos) in (&mut players, &mut positions).join() {
-        let destination_idx = map.xy_idx(pos.x + delta_x, pos.y + delta_y);
-        if map.tiles[destination_idx] != TileType::Wall {
-            pos.x = min(map.width - 1, max(0, pos.x + delta_x));
-            pos.y = min(map.height - 1, max(0, pos.y + delta_y));
-        }
-    }
-}
 
 struct State {
     ecs: World,
@@ -51,7 +22,7 @@ impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
 
-        player_input(self, ctx);
+        player::player_input(self, ctx);
         self.run_systems();
 
         let map = self.ecs.fetch::<Map>();
@@ -62,32 +33,6 @@ impl GameState for State {
 
         for (pos, render) in (&positions, &renderables).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
-        }
-    }
-}
-
-fn player_input(gs: &mut State, ctx: &mut BTerm) {
-    // Player movement
-    match ctx.key {
-        None => {} // Nothing happened
-        Some(key) => match key {
-            VirtualKeyCode::Left |
-            VirtualKeyCode::Numpad4 |
-            VirtualKeyCode::H => try_move_player(-1, 0, &mut gs.ecs),
-
-            VirtualKeyCode::Right |
-            VirtualKeyCode::Numpad6 |
-            VirtualKeyCode::L => try_move_player(1, 0, &mut gs.ecs),
-
-            VirtualKeyCode::Up |
-            VirtualKeyCode::Numpad8 |
-            VirtualKeyCode::K => try_move_player(0, -1, &mut gs.ecs),
-
-            VirtualKeyCode::Down |
-            VirtualKeyCode::Numpad2 |
-            VirtualKeyCode::J => try_move_player(0, 1, &mut gs.ecs),
-
-            _ => {}
         }
     }
 }
