42c3780b3f4659107c18846305352d0ae7730177
David Holmes <dholmes@dholmes.us>
2024-05-09 00:16:49 -0500

    2.4: Asking RLTK for a Viewshed: The System

--- src/visibility_system.rs
+++ src/visibility_system.rs
@@ -1,12 +1,20 @@
+use bracket_lib::prelude::{field_of_view, Point};
 use specs::prelude::*;
+use crate::map::Map;
 use super::{Viewshed, Position};
 
 pub struct VisibilitySystem {}
 
 impl<'a> System<'a> for VisibilitySystem {
-    type SystemData = (WriteStorage<'a, Viewshed>, WriteStorage<'a, Position>);
+    type SystemData = (ReadExpect<'a, Map>, WriteStorage<'a, Viewshed>, WriteStorage<'a, Position>);
 
-    fn run(&mut self, (mut viewshed, pos): Self::SystemData) {
-        for (viewshed, pos) in (&mut viewshed, &pos).join() {}
+    fn run(&mut self, data: Self::SystemData) {
+        let (map, mut viewshed, pos) = data;
+
+        for (viewshed, pos) in (&mut viewshed, &pos).join() {
+            viewshed.visible_tiles.clear();
+            viewshed.visible_tiles = field_of_view(Point::new(pos.x, pos.y), viewshed.range, &*map);
+            viewshed.visible_tiles.retain(|p| p.x >= 0 && p.x < map.width && p.y >= 0 && p.y < map.height);
+        }
     }
 }
