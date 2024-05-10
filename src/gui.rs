b3663046bf82ccdff43ff6b391fe96a1bce17f7b
David Holmes <dholmes@dholmes.us>
2024-05-09 22:30:44 -0500

    2.7: Adding a health bar

--- src/gui.rs
+++ src/gui.rs
@@ -1,6 +1,16 @@
 use bracket_lib::prelude::*;
 use specs::prelude::*;
+use crate::components::*;
 
 pub fn draw_ui(ecs: &World, ctx: &mut BTerm) {
     ctx.draw_box(0, 43, 79, 6, RGB::named(WHITE), RGB::named(BLACK));
+
+    let combat_stats = ecs.read_storage::<CombatStats>();
+    let players = ecs.read_storage::<Player>();
+    for (_player, stats) in (&players, &combat_stats).join() {
+        let health = format!(" HP {} / {} ", stats.hp, stats.max_hp);
+        ctx.print_color(12, 43, RGB::named(YELLOW), RGB::named(BLACK), &health);
+
+        ctx.draw_bar_horizontal(28, 43, 51, stats.hp, stats.max_hp, RGB::named(RED), RGB::named(BLACK));
+    }
 }
