00721362c1da2e0cc648377a2e8e70a3162d62d6
David Holmes <dholmes@dholmes.us>
2024-05-10 16:23:13 -0500

    2.8: Spawn All The Things

--- src/spawner.rs
+++ src/spawner.rs
@@ -1,7 +1,12 @@
-use bracket_lib::color::{BLACK, RGB, YELLOW};
-use bracket_lib::prelude::*;
+use bracket_lib::color::{BLACK, RED, RGB, YELLOW};
+use bracket_lib::prelude::{FontCharType, to_cp437};
+use bracket_lib::random::RandomNumberGenerator;
 use specs::prelude::*;
 use crate::components::{BlocksTile, CombatStats, Monster, Name, Player, Position, Renderable, Viewshed};
+use crate::map::{MAPWIDTH, Rect};
+
+const MAX_MONSTERS: i32 = 4;
+const MAX_ITEMS: i32 = 2;
 
 /// Spawns the player and returns their identity object.
 pub fn player(ecs: &mut World, player_x: i32, player_y: i32) -> Entity {
@@ -22,7 +27,7 @@ pub fn player(ecs: &mut World, player_x: i32, player_y: i32) -> Entity {
 
 /// Spawns a random monster at a given location
 pub fn random_monster(ecs: &mut World, x: i32, y: i32) {
-    let roll :i32;
+    let roll: i32;
     {
         let mut rng = ecs.write_resource::<RandomNumberGenerator>();
         roll = rng.roll_dice(1, 2);
@@ -34,20 +39,52 @@ pub fn random_monster(ecs: &mut World, x: i32, y: i32) {
 }
 
 fn orc(ecs: &mut World, x: i32, y: i32) { monster(ecs, x, y, to_cp437('o'), "Orc"); }
+
 fn goblin(ecs: &mut World, x: i32, y: i32) { monster(ecs, x, y, to_cp437('g'), "Goblin"); }
 
-fn monster<S : ToString>(ecs: &mut World, x: i32, y: i32, glyph : FontCharType, name : S) {
+fn monster<S: ToString>(ecs: &mut World, x: i32, y: i32, glyph: FontCharType, name: S) {
     ecs.create_entity()
-        .with(Position{ x, y })
-        .with(Renderable{
+        .with(Position { x, y })
+        .with(Renderable {
             glyph,
             fg: RGB::named(RED),
             bg: RGB::named(BLACK),
         })
-        .with(Viewshed{ visible_tiles : Vec::new(), range: 8, dirty: true })
-        .with(Monster{})
-        .with(Name{ name : name.to_string() })
-        .with(BlocksTile{})
-        .with(CombatStats{ max_hp: 16, hp: 16, defense: 1, power: 4 })
+        .with(Viewshed { visible_tiles: Vec::new(), range: 8, dirty: true })
+        .with(Monster {})
+        .with(Name { name: name.to_string() })
+        .with(BlocksTile {})
+        .with(CombatStats { max_hp: 16, hp: 16, defense: 1, power: 4 })
         .build();
+}
+
+/// Fills a room with stuff!
+pub fn spawn_room(ecs: &mut World, room: &Rect) {
+    let mut monster_spawn_points: Vec<usize> = Vec::new();
+
+    // Scope to keep the borrow checker happy
+    {
+        let mut rng = ecs.write_resource::<RandomNumberGenerator>();
+        let num_monsters = rng.roll_dice(1, MAX_MONSTERS + 2) - 3;
+
+        for _i in 0..num_monsters {
+            let mut added = false;
+            while !added {
+                let x = (room.x1 + rng.roll_dice(1, i32::abs(room.x2 - room.x1))) as usize;
+                let y = (room.y1 + rng.roll_dice(1, i32::abs(room.y2 - room.y1))) as usize;
+                let idx = (y * MAPWIDTH) + x;
+                if !monster_spawn_points.contains(&idx) {
+                    monster_spawn_points.push(idx);
+                    added = true;
+                }
+            }
+        }
+    }
+
+    // Actually spawn the monsters
+    for idx in monster_spawn_points.iter() {
+        let x = *idx % MAPWIDTH;
+        let y = *idx / MAPWIDTH;
+        random_monster(ecs, x as i32, y as i32);
+    }
 }
